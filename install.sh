1727dae45ec41d82fe4e5bbdeed1a6dc35ce3306
DevSugv <pyroheavenshop@gmail.com>
2025-08-09 18:23:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ SCRIPT_NAME="Lagify-Linux"
 INSTALL_PATH="/usr/local/bin/$SCRIPT_NAME"
 
 if [ ! -f "$SCRIPT_NAME" ]; then
-    echo "Error: $SCRIPT_NAME not found! Please place the script here and try again."
+    echo "Error: $SCRIPT_NAME not found..."
     exit 1
 fi
 
+chmod +x "$SCRIPT_NAME"
 sudo cp "$SCRIPT_NAME" "$INSTALL_PATH"
 sudo chmod +x "$INSTALL_PATH"
 
 echo "Installed $SCRIPT_NAME to $INSTALL_PATH"
 echo "You can now run it from the terminal, e.g. 'sudo Lagify-Linux'"
-
